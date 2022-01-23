e25f59f9365b0bb1b5feac4c3d390443286fda57
IshanShetty <ishan.h.shetty@gmail.com>
2022-01-22 20:50:30 -0600

    sidebar stuff

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,26 +5,36 @@ import { MatCardModule } from '@angular/material/card'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { LoginPageComponent } from './page/login-page/login-page.component';
 import { ConversationsPageComponent } from './page/conversations-page/conversations-page.component';
 
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
     AppComponent,
     ConversationsPageComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatCardModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
