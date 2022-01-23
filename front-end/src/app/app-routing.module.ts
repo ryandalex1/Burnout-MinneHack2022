a2b5895790c7f03ed994f7a11dc2de0b1e08f6b2
IshanShetty <ishan.h.shetty@gmail.com>
2022-01-22 19:56:35 -0600

    Login Page

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginPageComponent } from "./page/login-page/login-page.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LoginPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
