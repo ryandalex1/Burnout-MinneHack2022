be5d3ac98f5a90afc4a75d67a828f6b289595ca3
IshanShetty <ishan.h.shetty@gmail.com>
2022-01-22 20:00:58 -0600

    refactor directory structure

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ConversationsPageComponent } from './conversations-page/conversations-page.component';
+import { ConversationsPageComponent } from './page/conversations-page/conversations-page.component';
 import { LoginPageComponent } from "./page/login-page/login-page.component";
 
 const routes: Routes = [
   {
-    path: "chat",
-    component: ConversationsPageComponent
-  },
-  {
     path: '',
     pathMatch: 'full',
     component: LoginPageComponent,
-  }
+  },
+  {
+    path: "chat",
+    component: ConversationsPageComponent
+  },
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { MatCardModule } from '@angular/material/card'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ConversationsPageComponent } from './conversations-page/conversations-page.component';
 
 import { LoginPageComponent } from './page/login-page/login-page.component';
+import { ConversationsPageComponent } from './page/conversations-page/conversations-page.component';
+
 import { MatInputModule } from "@angular/material/input";
-import {MatButtonModule} from "@angular/material/button";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
